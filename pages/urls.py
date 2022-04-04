1653d0f9afbbe7c181d70c91002f805a8c7737da
meetshah <meetshah1724@gmail.com>
2022-04-04 15:29:51 +0530

    error resolve

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home, name='home'),
+    path('base/',views.base, name='base'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('services/',views.services,name='services'),
+    
+]
